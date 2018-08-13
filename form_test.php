887d8b2be96e9e17cb9ddf1bd1f30eba36a5b2dc
Miguel Adrian Domingo <miguelcdomingo@gmail.com>
2018-08-13 19:23:59 +0800

    final

--- form_test.php
+++ form_test.php
@@ -2,13 +2,22 @@
 <body>
 
 <?php 
-	$to = 'miguelcdomingo@gmail.com,hoppingsamurai@yahoo.com';
-	$name = $_POST["Name"];
-	$subject = $_POST["Purpose"];
-	$message = wordwrap($subject . "\n" . $_POST["Message"]);
-	$client_email_address = "From: " . $_POST["Email"] . "\r\n";
+if($_GLOBALS['REQUEST_METHOD'] == 'POST' || count($_POST) > 0) {
+		echo "Submitting Email...";
+		$to = 'miguelcdomingo@gmail.com,hoppingsamurai@yahoo.com';
+		$name = $_POST["Name"];
+		$subject = $_POST["Purpose"];
+		$client_email_address = "From: " . $_POST["Email"] . "\r\n";
+		$message = wordwrap($subject . "\n" . $client_email_address . "\n" . $_POST["Message"]);
 
-	mail($to, $subject, $message, $client_email_address);
+		$sendMail = mail($to, $subject, $message);
+		if ($sendMail){
+			echo "\n";
+			echo "Email Sent Successfully";
+		}
+
+}
+
 ?>
 
 </body>
