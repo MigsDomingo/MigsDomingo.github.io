c0eb545fb075fdd799a8b38428697f901a36d0ea
Miguel Adrian Domingo <miguelcdomingo@gmail.com>
2018-08-27 11:27:37 +0800

    Changes

--- form_test.php
+++ form_test.php
@@ -1,24 +1,25 @@
-<html>
-<body>
-
 <?php 
 if($_GLOBALS['REQUEST_METHOD'] == 'POST' || count($_POST) > 0) {
-		echo "Submitting Email...";
-		$to = 'miguelcdomingo@gmail.com,hoppingsamurai@yahoo.com';
+		$to = 'miguelcdomingo@gmail.com,asdi_manila@yahoo.com';
+		//$to = 'miguelcdomingo@gmail.com,hoppingsamurai@yahoo.com';
 		$name = $_POST["Name"];
-		$subject = $_POST["Purpose"];
-		$client_email_address = "From: " . $_POST["Email"] . "\r\n";
-		$message = wordwrap($subject . "\n" . $client_email_address . "\n" . $_POST["Message"]);
+		$subject = "Subject: [Website Inquiry] " . $_POST["Purpose"];
+		$client_email_address = "From Client Email Address: " . $_POST["Email"] . "\r\n";
+		$body = $_POST["Message"];
+		$message = wordwrap("\n" . $client_email_address . "\n" . $body);
 
 		$sendMail = mail($to, $subject, $message);
 		if ($sendMail){
-			echo "\n";
-			echo "Email Sent Successfully";
+			echo "<script type='text/javascript'>alert('Inquiry Submitted! Redirecting...'); window.location.replace(\"https://asdi.ph\"); </script>";
+			//echo "\n";
+			//echo "Email Sent Successfully. We will get back shortly to you.";
+			//header("Location: index.html");
+			//exit;
+		}
+		else {
+			echo "<script type='text/javascript'>alert('Submit Failed! Please check your network.');  window.location.replace(\"https://asdi.ph\");</script>";
 		}
 
 }
 
 ?>
-
-</body>
-</html>
